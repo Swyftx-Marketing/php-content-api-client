d87dd97e55eed44f4882845d31859a43f8fbe885
leewallis-swyftx <lee.wallis@swyftx.com.au>
2025-10-30 15:50:53 +1000

    Added in timestamps for story changes that were missing

--- src/Domain/Value/Link.php
+++ src/Domain/Value/Link.php
@@ -33,6 +33,9 @@ final readonly class Link
     public bool $isFolder;
     public bool $isStartPage;
     public bool $isPublished;
+    public string $publishedAt;
+    public string $createdAt;
+    public string $updatedAt;
 
     /**
      * @var list<LinkAlternate>
@@ -92,6 +95,15 @@ final readonly class Link
 
         Assert::keyExists($values, 'published');
         $this->isPublished = true === $values['published'];
+
+        Assert::keyExists($values, 'published_at');
+        $this->publishedAt = $values['published_at'];
+
+        Assert::keyExists($values, 'created_at');
+        $this->createdAt = $values['created_at'];
+
+        Assert::keyExists($values, 'updated_at');
+        $this->updatedAt = $values['updated_at'];
 
         $alternates = [];
 
